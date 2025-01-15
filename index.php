7414dfee2f6061689d43892cbe4752c088c2e500
David Ramos <davidrc0113gmail.com>
2025-01-15 13:59:23 +0100

    indecambio

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php 
-
- echo "hola mundo Daniel Perez";
- ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <h1>TecnoGear</h1>
+</body>
+</html>
